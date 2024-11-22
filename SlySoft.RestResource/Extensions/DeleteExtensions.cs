62449c04cc71649596767a8d81fb98b5ea4aed49
Jeff Sylvester <sylvesterjj@gmail.com>
2024-11-21 19:38:52 -0500

    adds ability to template delete links

--- SlySoft.RestResource/Extensions/DeleteExtensions.cs
+++ SlySoft.RestResource/Extensions/DeleteExtensions.cs
@@ -10,9 +10,10 @@ public static class DeleteExtensions {
     /// <param name="resource">The DELETE link will be added to this resource</param>
     /// <param name="name">Name of the element- will be converted to camelcase</param>
     /// <param name="href">HREF of the link</param>
+    /// <param name="templated">Whether or not the URI is templated</param>
     /// <returns>The resource so further calls can be chained</returns>
-    public static Resource Delete(this Resource resource, string name, string href) {
-        resource.Links.Add(new Link(name.ToCamelCase(), href, verb: "DELETE"));
+    public static Resource Delete(this Resource resource, string name, string href, bool templated = false) {
+        resource.Links.Add(new Link(name.ToCamelCase(), href, verb: "DELETE", templated: templated));
         return resource;
     }
 }
